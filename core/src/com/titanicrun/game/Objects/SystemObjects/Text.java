523f90be84dbea1468608be12676c8d8b154ca3f
ra0to <mikl181@yandex.ru>
2017-04-15 14:54:41 +0400

    Money

--- core/src/com/titanicrun/game/Objects/SystemObjects/Text.java
+++ core/src/com/titanicrun/game/Objects/SystemObjects/Text.java
@@ -1,7 +1,10 @@
 package com.titanicrun.game.Objects.SystemObjects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Vector2;
 import com.titanicrun.game.Objects.BaseObject;
 
@@ -10,11 +13,19 @@ import com.titanicrun.game.Objects.BaseObject;
  */
 
 public class Text extends BaseObject {
-    public String textValue = new String();
-    BitmapFont font = new BitmapFont();
+    public String textValue;
+    private BitmapFont font;
+    FreeTypeFontGenerator generator;
+    FreeTypeFontGenerator.FreeTypeFontParameter parameter;
     public Text (Vector2 position, String startText) {
         this.position = position;
         textValue = startText;
+        this.font = new BitmapFont();
+        this.generator = new FreeTypeFontGenerator(Gdx.files.internal("peepo.ttf"));
+        this.parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        this.parameter.size = 55;
+        this.parameter.color = new Color(0.95f, 0.92f, 0.03f, 1);
+        this.font = generator.generateFont(parameter);
     }
 
     @Override
@@ -25,8 +36,6 @@ public class Text extends BaseObject {
 
     @Override
     public void render(SpriteBatch spriteBatch) {
-        font.getData().setScale(3.5f);
-        font.setColor(0.95f, 0.92f, 0.03f, 1);
         font.draw(spriteBatch, textValue, position.x, position.y);
     }
 }
